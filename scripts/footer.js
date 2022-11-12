efad2f254ca2f1dcbda4b3d71732c569670bff53
Billy <kriper01@wp.pl>
2022-11-13 00:35:46 +0100

    fixed bubbles position after window resize

--- scripts/footer.js
+++ scripts/footer.js
@@ -6,7 +6,7 @@ function randomNum(min, max) {
 }
 
 function createBubbles() {
-    for (let i = 0; i < window.innerWidth / 20; i++) {
+    for (let i = 0; i < window.innerWidth / 30; i++) {
         footer.appendChild(createBubble());
     }
 }
@@ -17,7 +17,20 @@ function createBubble() {
     bubble.style.top = randomNum(0, 300) + "px";
     bubble.style.left = randomNum(0, window.innerWidth) + "px";
     bubble.style.scale = randomNum(10, 30) / 10;
-    bubble.style.animationDelay = randomNum(0, 70) / 10 + "s";
-    bubble.style.animationDuration = randomNum(90, 130) / 10 + "s";
+
+    let animDelay = randomNum(0, 70) / 10;
+    let animDuration = randomNum(90, 130) / 10;
+    bubble.style.animationDelay = animDelay + "s";
+    bubble.style.animationDuration = animDuration + "s";
+    setTimeout(resetPos, animDelay * 1000, animDuration * 1000, bubble);
+
     return bubble;
+}
+
+function resetPos(animDuration, bubble) {
+    setInterval(resetX, animDuration, bubble)
+}
+
+function resetX(bubble) {
+    bubble.style.left = randomNum(0, window.innerWidth) + "px";
 }
