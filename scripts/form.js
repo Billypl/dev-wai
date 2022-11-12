52354dbfa261b20ccf3d7bd1dc7c22b56d8a44d4
Billy <kriper01@wp.pl>
2022-11-12 04:11:00 +0100

    added form autosave

--- scripts/form.js
+++ scripts/form.js
@@ -1,7 +1,73 @@
-var slider = document.getElementById("monthly-beer");
-var output = document.getElementById("cur-val");
-output.innerHTML = slider.value;
+const formSlider = document.getElementById("monthly-beer");
+const formSliderOutput = document.getElementById("cur-val");
+formSliderOutput.innerHTML = formSlider.value;
 
-slider.oninput = function () {
-    output.innerHTML = this.value;
+formSlider.oninput = function () {
+    formSliderOutput.innerHTML = this.value;
+}
+
+
+const inputs = $(".questions input:not([type='radio'], [type='checkbox']), .questions select").toArray();
+const radios = $(".questions input[type='radio']").toArray();
+const checkboxes = $(".questions input[type='checkbox']").toArray();
+
+attachEventListeners();
+fillInputsAtTheStart();
+
+
+function attachEventListeners() {
+    for (radio of radios) {
+        radio.addEventListener("input", saveToSession);
+    }
+    for (input of inputs) {
+        input.addEventListener("input", saveToSession);
+    }
+    for (checkbox of checkboxes) {
+        checkbox.addEventListener("input", saveCheckboxesToSession);
+    }
+}
+
+function saveToSession() {
+    sessionStorage.setItem(this.name, this.value);
+}
+
+function saveCheckboxesToSession() {
+    let arr = [];
+    for (checkbox of checkboxes) {
+        if (checkbox.checked) {
+            arr.push(checkbox.value);
+        }
+    }
+    let json = JSON.stringify(arr);
+    sessionStorage.setItem(this.name, json);
+}
+
+
+function fillInputsAtTheStart() {
+    fillGeneralInputs();
+    fillRadioButtons();
+    fillCheckboxes();
+}
+
+function fillGeneralInputs() {
+    for (input of inputs) {
+        let val = sessionStorage.getItem(input.name);
+        input.value = val;
+    }
+}
+
+function fillRadioButtons() {
+    for (radio of radios) {
+        let val = sessionStorage.getItem(radio.name);
+        if (radio.value == val)
+            radio.checked = true;
+    }
+}
+
+function fillCheckboxes() {
+    for (checkbox of checkboxes) {
+        let arr = JSON.parse(sessionStorage.getItem(checkbox.name));
+        if (arr.includes(checkbox.value))
+            checkbox.checked = true;
+    }
 }
