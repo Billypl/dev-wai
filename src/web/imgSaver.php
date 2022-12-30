9f03b9bb81ee98dd3bcae6899f9c8ae185e84131
Billy <kriper01@wp.pl>
2022-12-30 20:57:45 +0100

    added category 1.A

--- src/web/imgSaver.php
+++ src/web/imgSaver.php
@@ -1,6 +1,12 @@
 <?php
     include_once "imgValidator.php";
     const uploadDir = '/var/www/dev/src/web/upload/';
+    const imgUploadDir = uploadDir.'img/';
+    const thumbUploadDir = uploadDir.'thumb/';
+    const watermarkUploadDir = uploadDir.'watermark/';
+
+
+
     checkForIllegalDirectAccess();
     $file = $_FILES['img'];
 
@@ -15,12 +21,37 @@
         header("Location: index.php");
         exit;
     }
+    
     function saveFile($file)
     {
         $filename = basename($file['name']);
-        $target = uploadDir.$filename;
         $tmpPath = $file['tmp_name'];
+        saveImg($filename, $tmpPath);
+        saveThumb($filename, $tmpPath);
+    }
 
+    function saveImg($filename, $tmpPath)
+    {
+        $target = imgUploadDir.$filename;
         $success = move_uploaded_file($tmpPath, $target);
         setcookie("isFileSent", $success);
+    }
+
+    function saveThumb($filename, $tmpPath)
+    {
+        $target = thumbUploadDir.$filename;
+        $jpg_image = copyImage($filename);
+
+        $thumb = imagescale($jpg_image, 200, 125);
+        imagepng($thumb, $target,9);
+
+    }
+
+    function copyImage($filename)
+    {
+        $type = substr($filename,strrpos($filename,'.')+1);
+        if($type == "jpeg" || $type == "jpg")
+            return imagecreatefromjpeg(imgUploadDir.$filename);
+        elseif ($type == "png")
+            return imagecreatefrompng(imgUploadDir.$filename);
     }
